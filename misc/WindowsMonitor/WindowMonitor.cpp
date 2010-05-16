941ec6371ffdd13e92a82b7080cc5a77b01eb369
angelipin <angelipin@7ff612aa-105e-4e35-adfd-5cdac6c44c12>
2010-05-16 16:09:49 +0000

    Change Compare Title to Compare Hwnd. This will works berrer when title often changes.

--- misc/WindowsMonitor/WindowMonitor.cpp
+++ misc/WindowsMonitor/WindowMonitor.cpp
@@ -8,7 +8,7 @@
 #include <cassert>
 #include <vector>
 CRITICAL_SECTION CriticalSection;
-CRITICAL_SECTION cs;
+
 WindowMonitor* g_pThis = 0;
 //--------------------------------------------------------------------
 // Constructor and destructor
@@ -25,19 +25,12 @@ WindowMonitor::~WindowMonitor()
 
 WCHAR black_list[256][256];
 WCHAR black_process[256][256];
-typedef struct HWNDLIST
-{
-	HWND hwnd;
-	int count; 
-}HWNDL;
 
-HWNDL hwndlist[4096];
 
 int list_len = 0;
 int process_len = 0;
-static int hwndlist_len = 0;
 
-FILE * fq;
+
 //--------------------------------------------------------------------
 // Methods
 //--------------------------------------------------------------------
@@ -47,8 +40,6 @@ bool WindowMonitor::Initialize()
 
 	m_Log.SetFilename( L"WindowMonitorLog.xml" );
 
-	m_strPipename =  L"\\\\.\\pipe\\ShadoWin.WindowsMon";
-
 	m_hWindowStation = GetProcessWindowStation();
 	if ( m_hWindowStation == 0 )
 	{
@@ -56,9 +47,6 @@ bool WindowMonitor::Initialize()
 	}
 	EnumDesktop();
 
-	//fq = fopen("windows_monitor.log.txt","a");
-	//fwprintf(fq,L"Start.\n");
-	//fclose(fq);
     FILE * fp = fopen("black_list.config.txt","r");
 	char t [256];
 	WCHAR wt [256];
@@ -112,18 +100,11 @@ bool WindowMonitor::Initialize()
 	process_len = count;
 	fclose(fp);
     
-	for(int i = 0 ; i < 4096 ; i ++)
-	{
-		hwndlist[i].count = 0;
-		hwndlist[i].hwnd = NULL;
-	}
+
 	if (!InitializeCriticalSectionAndSpinCount(&CriticalSection, 
         0x80000400) ) 
         return false;
 
-	if (!InitializeCriticalSectionAndSpinCount(&cs, 
-        0x80000400) ) 
-        return false;
 	
 	HANDLE hMonitor = 
 	CreateThread(	0,
@@ -162,33 +143,20 @@ unsigned int __stdcall WindowMonitor::ShowPopupWindowThread(LPVOID lpParam)
 	GetUserObjectInformation(dsk,UOI_NAME,desktopname,len + 1,&len);
 	//Ìí¼ÓºÚÃûµ¥
 	
-	if(lstrcmp(L"Shadowin2010 (Running) - Microsoft Visual Studio",p->title)==0 ||
-		lstrcmp(L"Shadowin2010 (Debugging) - Microsoft Visual Studio",p->title)==0 || 
-		lstrcmp(L"ShadowinPopUpMsg",p->title)==0 ||
+	if(lstrcmp(L"ShadowinPopUpMsg",p->title)==0 ||
 		lstrcmp(L"Default&Debug",p->title)==0 ||  
-		lstrcmp(L"SysFader",p->title)==0 //||
-		//lstrcmp(L"",p->title)==0 || StrStr(p->title,L"Shadowin") != NULL )
+		lstrcmp(L"SysFader",p->title)==0 
 		)
 	{
 		return 0; 
 	}
 	WCHAR cmd[2046];
-	/*
-	lstrcpy(cmd,L"");
-	lstrcat(cmd, L"E:\\ppp\\Release\\ShadowinPopUpMsg.exe \"");
-	lstrcat(cmd,p->title);
-	lstrcat(cmd,L"\" ");
-	lstrcat(cmd,p->desktop);
-	cmd = 0x00feef80 "ShadowinPopUpMsg.exe "SysFader" Default"
-	*/
 	WCHAR title[1024];
 	lstrcpy(title,p->title);
 
 	StrTrim(title,L"\"!@#$%^&*()_+|-=\\?/><'");
 	title[20] = 0;
 	wsprintf(cmd,L"ShadowinPopUpMsg.exe \"%s...\" %s",title,p->desktop);
-	/*FILE * f = fopen("windows_monitor.log.txt","a");
-	fwprintf(f,L"Title is %s. Desktop is %s\n",title,p->desktop);*/
 	
 	if(lstrcmp(p->desktop,(LPWSTR)desktopname) != 0 && lstrcmp(p->desktop,L"Default") == 0)
 	{
@@ -202,8 +170,6 @@ unsigned int __stdcall WindowMonitor::ShowPopupWindowThread(LPVOID lpParam)
 	
 		int ret = CreateProcess(NULL,cmd,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi);	
 	}
-	//free(lpParam);
-	/*fclose(f);*/
 	CloseDesktop(dsk);
 	LeaveCriticalSection(&CriticalSection);
 	return 0;
@@ -213,25 +179,21 @@ int WindowMonitor::Transport2Server()
 {
 	wstring strBuffer;
 	
-	CXMLNode* node_root = CXMLNode::Create( L"root" );
+	//CXMLNode* node_root = CXMLNode::Create( L"root" );
 
-	if ( node_root == 0 )
-	{
-		return 0;
-	}
+	//if ( node_root == 0 )
+	//{
+	//	return 0;
+	//}
 	for ( size_t i = 0; i < m_WindowAttributeVec.size(); ++i )
 	{
-		CXMLNode* node_desktop = 
-		CXMLNode::Create( L"desktop", m_WindowAttributeVec[ i ].strAttachDesktop );
-		CXMLNode* node_title = 
-		CXMLNode::Create( L"title", m_WindowAttributeVec[ i ].strWindowTitle );
+		//CXMLNode* node_desktop = 
+		//CXMLNode::Create( L"desktop", m_WindowAttributeVec[ i ].strAttachDesktop );
+		////CXMLNode* node_title = 
+		//CXMLNode::Create( L"title", m_WindowAttributeVec[ i ].strWindowTitle );
 		MSGDATA * msg = (PMSGDATA)malloc(sizeof(MSGDATA));
 		lstrcpy((LPWSTR)(msg->desktop),(LPCWSTR)m_WindowAttributeVec[ i ].strAttachDesktop.c_str());
 		lstrcpy((LPWSTR)(msg->title),(LPCWSTR)m_WindowAttributeVec[ i ].strWindowTitle.c_str());
-		
-		/*for( int i = 0 ; i < count ; i ++ )
-		{
-		}*/
 		
 		bool isInBlack(false);
 		for( int i = 0 ; i < list_len ; i ++)
@@ -247,35 +209,11 @@ int WindowMonitor::Transport2Server()
 			DWORD tid;
 			CreateThread(NULL,0,( LPTHREAD_START_ROUTINE )WindowMonitor::ShowPopupWindowThread,msg,0,&tid);
 		}
-		node_root->InsChild( node_desktop );
-		node_desktop->InsChild( node_title );
+		//node_root->InsChild( node_desktop );
+		//node_desktop->InsChild( node_title );
 	}
-	node_root->Output( strBuffer );
+	//node_root->Output( strBuffer );
 
-#ifdef _DEBUG
-	//m_Log.InitRoot( node_root );
-	//m_Log.SaveFile(
-	//::GetMainDirectory() + L"windowmonitorlog.xml" );
-#endif
-    /*
-	{"Type":0,"NameSpace":"Shadowin.VEManager","ClassName":"VEManager","MethodName":"Switch","Prameters":["b46756c5-0db3-4540-8d54-56839ac36d9a"]}
-
-	const int iBufferSize = 512;
-	
-	DWORD dwRead = 0;
-
-	char* pRecvBuffer = new char[ iBufferSize ];
-	BOOL bReturn = 
-	CallNamedPipe(	m_strPipename.c_str(),
-					( LPVOID )strBuffer.c_str(),
-					( strBuffer.size() + 1 )*sizeof( wchar_t ) ,
-					pRecvBuffer,
-					iBufferSize,
-					&dwRead,
-					NMPWAIT_NOWAIT );
-					//NMPWAIT_WAIT_FOREVER );
-	Safe_Delete_Array( pRecvBuffer );
-    */
 	m_WindowAttributeVec.clear();
 	return 0;
 }
@@ -283,6 +221,7 @@ int WindowMonitor::EnumNewWindows(	const wstring& name,
 									TitleMap& cur, 
 									TitleMap& pre )
 {
+	static const unsigned int uiBufferSize = 256;
 	if ( pre.empty() )
 	{
 		pre = cur;
@@ -295,10 +234,17 @@ int WindowMonitor::EnumNewWindows(	const wstring& name,
 	{
 		if ( pre.find( cur_iter->first ) == pre.end() )
 		{
-			WindowAttribute waTemp;
-			waTemp.strAttachDesktop = name;
-			waTemp.strWindowTitle =  cur_iter->first;
-			m_WindowAttributeVec.push_back( waTemp );
+			wchar_t strWindowText[ uiBufferSize ];
+			ZeroMemory( strWindowText, sizeof( wchar_t )*uiBufferSize );
+			int ret = GetWindowText( cur_iter->first, strWindowText, uiBufferSize );
+			if(!ret)
+			{
+				
+				WindowAttribute waTemp;
+				waTemp.strAttachDesktop = name;
+				waTemp.strWindowTitle = strWindowText;
+				m_WindowAttributeVec.push_back( waTemp );
+			}
 		}
 	}
 	pre.clear();
@@ -383,7 +329,7 @@ unsigned int __stdcall WindowMonitor::MonitorThread( void* param )
 {
 	assert( g_pThis );
 	SetTimer( 0, 0, g_pThis->GetEnumInterval(), ( TIMERPROC )( TimerCallback ) );
-	SetTimer( 0, 0, 30 * 1000, ( TIMERPROC )CleanHwnd  );
+
 	MSG msg;
 	while ( GetMessage( &msg, 0, 0, 0 ) )
 	{
@@ -433,14 +379,7 @@ unsigned int __stdcall WindowMonitor::EnumDesktopWindowsCallback( HWND hwnd, uns
 	{
 		return WindowMonitor::c_uiEnumFunctionReturn;
 	}
-	//check the process
-	//HwndList
-	/*if(HwndList.)
-	{
-		return  WindowMonitor::c_uiEnumFunctionReturn;
-	}
-	HwndList.insert(pair<HWND, bool>(hwnd, true));*/
-	//hwndlist
+
 
 	DWORD pid;
 	GetWindowThreadProcessId(hwnd,&pid);
@@ -478,57 +417,27 @@ unsigned int __stdcall WindowMonitor::EnumDesktopWindowsCallback( HWND hwnd, uns
 		}
 		CloseHandle( handle );
 	}
-	wchar_t strWindowText[ uiBufferSize ];
+	/*wchar_t strWindowText[ uiBufferSize ];
 
 	ZeroMemory( strWindowText, sizeof( wchar_t )*uiBufferSize );
 
 	GetWindowText( hwnd, strWindowText, uiBufferSize );
 
-	std::wstring strText( strWindowText );	
+	std::wstring strText( strWindowText );	*/
 
- //   EnterCriticalSection(&cs);
-	//for(int i = 0 ; i < hwndlist_len ; i ++ )
-	//{
-	//	if(hwndlist[i].hwnd == hwnd)
-	//	{
-	//		if( hwndlist[i].count > 1)//hlc
-	//		{
-	//			return WindowMonitor::c_uiEnumFunctionReturn;
-	//		}
-	//		hwndlist[i].count ++;
-	//	}
-	//}
 
-	//if(hwndlist_len > 4096 )
-	//{
-	//	hwndlist_len = 4095;
-	//}
-	//hwndlist[hwndlist_len].hwnd = hwnd;
-	//hwndlist_len ++;
-	//LeaveCriticalSection(&cs);
-
-	if ( strText.size() )
+	/*if ( strText.size() )*/
 	{
 		vector< DesktopAttribute >& rDesktopVec = g_pThis->GetDesktopVec();
 
-		if ( !rDesktopVec[ uiDesktopIndex ].PreviousTitles[ strText ] )
+		if ( !rDesktopVec[ uiDesktopIndex ].PreviousTitles[ hwnd ] )
 		{
-			rDesktopVec[ uiDesktopIndex ].PreviousTitles[ strText ] = true;
+			rDesktopVec[ uiDesktopIndex ].PreviousTitles[ hwnd ] = true;
 		}
 	}
 	return WindowMonitor::c_uiEnumFunctionReturn;
 }
-void __stdcall WindowMonitor::CleanHwnd( HWND, UINT, UINT_PTR, DWORD )
-{
-	EnterCriticalSection(&cs);
-	for(int i = 0 ; i < 4096 ; i ++)
-	{
-		hwndlist[i].count = 0;
-		hwndlist[i].hwnd = NULL;
-	}
-	hwndlist_len = 0;
-	LeaveCriticalSection(&cs);
-}
+
 void __stdcall WindowMonitor::TimerCallback( HWND, UINT, UINT_PTR, DWORD )
 {
 	assert( g_pThis );
